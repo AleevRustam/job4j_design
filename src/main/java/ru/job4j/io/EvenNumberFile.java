89f602252358aa4187b6169e440957492281c7be
AleevRustam <re1973@mail.ru>
2024-05-08 13:00:53 +0200

    2.1. FileInputStream [#4898 #494888]

--- src/main/java/ru/job4j/io/EvenNumberFile.java
+++ src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -1,6 +1,7 @@
 package ru.job4j.io;
 
 import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -24,7 +25,7 @@ public class EvenNumberFile {
                 }
             }
 
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
