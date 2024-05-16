a92c10a4dca1187a64f1eadeadc05e39e8c3a844
AleevRustam <re1973@mail.ru>
2024-05-16 09:48:34 +0200

    7. Анализ доступности сервера [#859 #496087]

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -1,53 +1,37 @@
 package ru.job4j.io;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Analysis {
+
     public static void unavailable(String source, String target) {
-        List<String> logs = new ArrayList<>();
-        boolean notWorked = false;
-        StringBuilder result = new StringBuilder();
-        
-        try (BufferedReader input = new BufferedReader(new FileReader(source))) {
-            logs = input.lines().toList();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        try (BufferedReader input = new BufferedReader(new FileReader(source));
+             PrintWriter output = new PrintWriter(new FileWriter(target))) {
 
-        String status = stringSplit(logs.get(0))[0];
-        String time = stringSplit(logs.get(0))[1];
-        if (!isWorking(status)) {
-            notWorked = true;
-            result.append(time).append(";");
-        }
-        for (int i = 1; i < logs.size(); i++) {
-            status = stringSplit(logs.get(i))[0];
-            time = stringSplit(logs.get(i))[1];
-            if (isWorking(status) && notWorked) {
-                result.append(time).append(";").append(System.lineSeparator());
-                notWorked = false;
+            String line;
+            boolean notWorked = false;
+            String startTime = "";
+            String endTime;
+
+            while ((line = input.readLine()) != null) {
+                String[] parts = line.split(" ");
+                String status = parts[0];
+                String time = parts[1];
+                if (!isWorking(status) && !notWorked) {
+                    notWorked = true;
+                    startTime = time;
+                } else if (isWorking(status) && notWorked) {
+                    endTime = time;
+                    output.println(startTime + ";" + endTime + ";");
+                    startTime = "";
+
+                    notWorked = false;
+                }
             }
-            if (!isWorking(status) && !notWorked) {
-                notWorked = true;
-                result.append(time).append(";");
-            }
-        }
-        try (PrintWriter output = new PrintWriter(
-                new BufferedOutputStream(
-                        new FileOutputStream(target)
-                ))) {
-            output.print(result);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
-    private static String[] stringSplit(String string) {
-        return string.split(" ");
-    }
-
     private static boolean isWorking(String string) {
         return string.equals("200") || string.equals("300");
     }
