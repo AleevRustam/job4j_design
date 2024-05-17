fc49dd960015e792afa00061f4030d5383bc6b5d
AleevRustam <re1973@mail.ru>
2024-05-17 11:58:29 +0200

    7. Анализ доступности сервера [#859 #496087]

--- src/main/java/ru/job4j/io/Analysis.java
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -3,31 +3,32 @@ package ru.job4j.io;
 import java.io.*;
 
 public class Analysis {
-
     public static void unavailable(String source, String target) {
         try (BufferedReader input = new BufferedReader(new FileReader(source));
              PrintWriter output = new PrintWriter(new FileWriter(target))) {
 
+            boolean serverDone = false;
+            String startTime = null;
             String line;
-            boolean notWorked = false;
-            String startTime = "";
-            String endTime;
 
             while ((line = input.readLine()) != null) {
                 String[] parts = line.split(" ");
                 String status = parts[0];
                 String time = parts[1];
-                if (!isWorking(status) && !notWorked) {
-                    notWorked = true;
-                    startTime = time;
-                } else if (isWorking(status) && notWorked) {
-                    endTime = time;
-                    output.println(startTime + ";" + endTime + ";");
-                    startTime = "";
 
-                    notWorked = false;
+                if (!isWorking(status)) {
+                    if (!serverDone) {
+                        serverDone = true;
+                        startTime = time;
+                    }
+                } else {
+                    if (serverDone) {
+                        serverDone = false;
+                        output.printf("%s;%s;%n", startTime, time);
+                    }
                 }
             }
+
         } catch (IOException e) {
             e.printStackTrace();
         }
