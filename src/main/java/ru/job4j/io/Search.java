4fdee2266baca90bb13dd23608751c42c6ea8f5a
AleevRustam <re1973@mail.ru>
2024-05-28 10:39:34 +0200

    12. Валидация параметров запуска [#246865 #498095]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -10,13 +10,23 @@ import java.util.function.Predicate;
 public class Search {
     public static void main(String[] args) throws IOException {
         isValid(args);
-        Path start = Paths.get(args[0]);
-        search(start, path -> path.toFile().getName().endsWith(args[1])).forEach(System.out::println);
+        Path startDirectory = Paths.get(args[0]);
+        search(startDirectory, path -> path.toFile().getName().endsWith(args[1])).forEach(System.out::println);
     }
 
     private static void isValid(String[] args) {
         if (args.length < 2) {
             throw new IllegalArgumentException("Need two arguments: start folder and file extension");
+        } else {
+            Path startDirectory = Paths.get(args[0]);
+            if (!Files.exists(startDirectory)
+                    || !Files.isDirectory(startDirectory)
+                    || !Files.isReadable(startDirectory)) {
+                throw new IllegalArgumentException("Error: first argument must be a start folder");
+            }
+            if (!args[1].matches("\\\\.[A-Za-z0-9]{1,3}")) {
+                throw new IllegalArgumentException("Error: second argument must be a file extension");
+            }
         }
     }
 
