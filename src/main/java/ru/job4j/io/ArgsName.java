78372a985b6048d92aab5d4bbc41cd7546293201
AleevRustam <re1973@mail.ru>
2024-05-29 10:44:37 +0200

    13. Именованные аргументы [#295518 #498256]

--- src/main/java/ru/job4j/io/ArgsName.java
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -10,41 +10,43 @@ public class ArgsName {
     private final Map<String, String> values = new HashMap<>();
 
 
-
     public String get(String key) {
         if (values.containsKey(key)) {
-            return  values.get(key);
+            return values.get(key);
         } else {
             throw new IllegalArgumentException("This key: '" + key + "' is missing");
         }
     }
 
     private void parse(String[] args) {
+        for (String arg : args) {
+            String[] keyValue = validateAndExtract(arg);
+            values.put(keyValue[0], keyValue[1]);
+        }
+    }
+
+    private String[] validateAndExtract(String arg) {
         String regEx = "^-([a-zA-Z0-9_.-]+)=(.+)$";
         Pattern pattern = Pattern.compile(regEx);
-        Matcher matcher;
-        for (String arg : args) {
-            matcher = pattern.matcher(arg);
-            if (matcher.matches()) {
-                values.put(matcher.group(1), matcher.group(2));
-            } else {
-                if (!arg.startsWith("-")) {
-                    throw new IllegalArgumentException("Error: This argument '" + arg + "' does not start with a '-' character");
-                }
+        Matcher matcher = pattern.matcher(arg);
 
-                if (!arg.contains("=")) {
-                    throw new IllegalArgumentException("Error: This argument '" + arg + "' does not contain an equal sign");
-                }
-
-                String[] parts = arg.split("=", 2);
-
-                if (parts[0].length() < 2) {
-                    throw new IllegalArgumentException("Error: This argument '" + arg + "' does not contain a key");
-                }
-                if (parts[1].isEmpty()) {
-                    throw new IllegalArgumentException("Error: This argument '" + arg + "' does not contain a value");
-                }
+        if (matcher.matches()) {
+            return new String[]{matcher.group(1), matcher.group(2)};
+        } else {
+            if (!arg.startsWith("-")) {
+                throw new IllegalArgumentException("Error: This argument '" + arg + "' does not start with a '-' character");
             }
+            if (!arg.contains("=")) {
+                throw new IllegalArgumentException("Error: This argument '" + arg + "' does not contain an equal sign");
+            }
+            String[] parts = arg.split("=", 2);
+            if (parts[0].length() < 2) {
+                throw new IllegalArgumentException("Error: This argument '" + arg + "' does not contain a key");
+            }
+            if (parts[1].isEmpty()) {
+                throw new IllegalArgumentException("Error: This argument '" + arg + "' does not contain a value");
+            }
+            return parts;
         }
     }
 
@@ -60,10 +62,10 @@ public class ArgsName {
     }
 
     public static void main(String[] args) {
-        ArgsName jvm = ArgsName.of(new String[] {"-Xmx=512", "-encoding=UTF-8"});
+        ArgsName jvm = ArgsName.of(new String[]{"-Xmx=512", "-encoding=UTF-8"});
         System.out.println(jvm.get("Xmx"));
 
-        ArgsName zip = ArgsName.of(new String[] {"-out=project.zip", "-encoding=UTF-8"});
+        ArgsName zip = ArgsName.of(new String[]{"-out=project.zip", "-encoding=UTF-8"});
         System.out.println(zip.get("out"));
     }
 }
