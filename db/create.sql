385eb6106e4f048cd30dc12ab0dad8b3ad9c08f3
AleevRustam <re1973@mail.ru>
2024-07-10 14:04:03 +0200

    2. Схема прав пользователи и роли. [#1093]

---
+++ db/create.sql
@@ -0,0 +1,49 @@
+CREATE TABLE comments(
+	id serial primary key,
+	name varchar(255)
+);
+
+CREATE TABLE attachs(
+	id serial primary key,
+	name varchar(255)
+);
+
+CREATE TABLE items(
+	id serial primary key,
+	name varchar(255),
+	comment_id int REFERENCES comments(id),
+	attach_id int REFERENCES attachs(id)
+);
+
+CREATE TABLE categories(
+	id serial primary key,
+	name varchar(255),
+	item_id int REFERENCES items(id)
+);
+
+CREATE TABLE states(
+	id serial primary key,
+	name varchar(255),
+	item_id int REFERENCES items(id)
+);
+
+
+CREATE TABLE users(
+	id serial primary key,
+	name varchar(255),
+	item_id int REFERENCES items(id)
+);
+
+CREATE TABLE roles(
+	id serial primary key,
+	name varchar(255),
+	user_id int REFERENCES users(id)
+);
+
+CREATE TABLE rules(
+	id serial primary key,
+	name varchar(255),
+	role_id int REFERENCES roles(id)
+);
+
+ALTER TABLE roles ADD COLUMN rule_id int REFERENCES rules(id);
---
+++ db/insert.sql
@@ -0,0 +1,32 @@
+INSERT INTO comments(name) VALUES ('Comment 1');
+INSERT INTO comments(name) VALUES ('Comment 2');
+INSERT INTO comments(name) VALUES ('Comment 3');
+
+INSERT INTO attachs(name) VALUES ('Attachment 1');
+INSERT INTO attachs(name) VALUES ('Attachment 2');
+INSERT INTO attachs(name) VALUES ('Attachment 3');
+
+INSERT INTO items(name, comment_id, attach_id) VALUES ('Item 1', 1, 1);
+INSERT INTO items(name, comment_id, attach_id) VALUES ('Item 2', 2, 2);
+INSERT INTO items(name, comment_id, attach_id) VALUES ('Item 3', 3, 3);
+
+INSERT INTO categories(name, item_id) VALUES ('Category 1', 1);
+INSERT INTO categories(name, item_id) VALUES ('Category 2', 2);
+INSERT INTO categories(name, item_id) VALUES ('Category 3', 3);
+
+INSERT INTO states(name, item_id) VALUES ('State 1', 1);
+INSERT INTO states(name, item_id) VALUES ('State 2', 2);
+INSERT INTO states(name, item_id) VALUES ('State 3', 3);
+
+INSERT INTO users(name, item_id) VALUES ('User 1', 1);
+INSERT INTO users(name, item_id) VALUES ('User 2', 2);
+INSERT INTO users(name, item_id) VALUES ('User 3', 3);
+
+INSERT INTO roles(name) VALUES ('Role 1');
+INSERT INTO roles(name) VALUES ('Role 2');
+INSERT INTO roles(name) VALUES ('Role 3');
+
+INSERT INTO rules(name, role_id) VALUES ('Rule 1', 1);
+INSERT INTO rules(name, role_id) VALUES ('Rule 2', 2);
+INSERT INTO rules(name, role_id) VALUES ('Rule 3', 3);
+
